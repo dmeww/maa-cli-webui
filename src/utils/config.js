ff0b0ec4952434c42524ef5fcd011fbc653869a8
!Dyg200202 <dygticky@gmail.com>
2024-04-22 10:18:08 +0800

    change api to window.origin

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@
  * @type {{server: string}}
  */
 const Config = {
-    server: `http://10.0.0.104/api`
+    server: `${window.location.origin}/api`
 }
 // http://10.0.0.110:3000/api
 // `${window.location.origin}/api`
